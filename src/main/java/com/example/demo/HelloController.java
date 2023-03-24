d029d71e0b7623172c729d5532bc637241a2cf2f
Juanan Pereira <juanan.pereira@ehu.eus>
2023-03-24 10:20:20 +0100

    Verbose mode

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -16,11 +16,20 @@ public class HelloController {
     protected void onHelloButtonClick() {
 
 
-
         Utils.asyncTask(
-                () ->
-                        new Image("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"),
-                image -> picture.setImage(image));
+                new Utils.ProducerWithThrow<Image>() {
+                    @Override
+                    public Image apply() {
+                        return new Image("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png");
+                    }
+                },
+                new Utils.Consumer<Image>() {
+                    @Override
+                    public void apply(Image image) {
+                        picture.setImage(image);
+                    }
+                }
+        );
 
         welcomeText.setText("Welcome to JavaFX Application!"
 
