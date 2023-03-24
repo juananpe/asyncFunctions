e4fc09278e69f0f38d3a5603fba21b77a1c351c8
Juanan Pereira <juanan.pereira@ehu.eus>
2023-03-24 11:42:03 +0100

    Add a textfield to check the benefits of an async operation

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField textfield;
 
     @FXML
     private ImageView picture;
